d766323a8fd03807130b81fc4c91077e799de32a
jj <john-git@ofjj.net>
2013-11-03 01:20:49 +0100

    csv parser

--- csv.cpp
+++ csv.cpp
@@ -19,7 +19,7 @@ private:
 	unsigned buf_cur;
 	unsigned buf_end;
 	unsigned buf_size;
-	unsigned char *buf;
+	char *buf;
 
 	// move buf_cur to buf_start, fill buf_end..buf_size with freshly read data
 	void refill_buffer ( )
@@ -36,7 +36,7 @@ private:
 
 		if ( buf_end < buf_size )
 		{
-			input.read( (char*)(buf + buf_end), buf_size - buf_end );
+			input.read( buf + buf_end, buf_size - buf_end );
 			buf_end += input.gcount();
 			if (buf_end > buf_size)
 				buf_end = buf_cur;
@@ -44,6 +44,7 @@ private:
 	}
 
 public:
+	// return true if no more data is available from input
 	bool eos ( )
 	{
 		if ( input.good() )
@@ -55,22 +56,19 @@ public:
 		return true;
 	}
 
-	explicit line_reader ( istream &is, int line_max=1<<24 ) :
+	explicit line_reader ( istream &is, unsigned line_max=1<<20 ) :
 		input(is),
 		buf_cur(0),
 		buf_end(0),
 		buf_size(line_max)
 	{
-		buf = new unsigned char[buf_size];
+		buf = new char[buf_size];
 
-		input.read( (char*)buf, buf_size );
+		input.read( buf, (buf_size > 4096 ? buf_size/16 : buf_size) );
 		buf_end = input.gcount();
 
-		if (buf_end > buf_size)
-			buf_end = buf_cur;	// just in case
-
 		// discard utf-8 BOM
-		if ( (buf_end >= 3) && buf[0] == 0xef && buf[1] == 0xbb && buf[2] == 0xbf )
+		if ( (buf_end >= 3) && buf[0] == '\xef' && buf[1] == '\xbb' && buf[2] == '\xbf' )
 			buf_cur += 3;
 	}
 
@@ -81,14 +79,15 @@ public:
 
 	// read one line from input, starting at buf_cur
 	// returns the line start in line_start and the line length in line_length
-	// line_length does not include the newline character(s)
+	// line_length includes the newline character(s)
 	// the line is not null-terminated
 	// internally, advances buf_cur to the beginning of next line
 	// returns line_start = NULL if no newline is found
 	// eof is considered as a newline
-	void read_line ( unsigned char* &line_start, unsigned &line_length )
+	// the returned pointers are only valid until the next call to read_line
+	void read_line ( char* &line_start, unsigned &line_length )
 	{
-		unsigned char *nl = (unsigned char*)memchr( (void*)(buf + buf_cur), '\n', buf_end - buf_cur );
+		char *nl = (char*)memchr( (void*)(buf + buf_cur), '\n', buf_end - buf_cur );
 
 		// newline found ?
 		if ( nl )
@@ -100,9 +99,6 @@ public:
 			if (buf_cur > buf_end)
 				buf_cur = buf_end;	// just in case
 
-			if ( line_length >= 1 && line_start[ line_length - 1 ] == '\r' )
-				--line_length;
-
 			return;
 		}
 
@@ -113,9 +109,6 @@ public:
 			line_length = buf_end - buf_cur;
 
 			buf_cur = buf_end;
-
-			if ( line_length >= 1 && line_start[ line_length - 1 ] == '\r' )
-				--line_length;
 
 			return;
 		}
@@ -141,6 +134,195 @@ public:
 	}
 };
 
+class csv_reader
+{
+private:
+	line_reader *input_lines;
+	unsigned line_max;
+	char *line_copy;
+
+	char sep;
+	char quot;
+
+	char *cur_line;
+	unsigned cur_line_length;
+	unsigned cur_line_length_nl;
+	unsigned cur_field_offset;
+
+	// set cur_line_length from cur_line_length_nl, trim \r\n
+	void trim_newlines ( )
+	{
+		cur_line_length = cur_line_length_nl;
+
+		if ( cur_line_length > 0 && cur_line[ cur_line_length - 1 ] == '\n' )
+			cur_line_length--;
+
+		if ( cur_line_length > 0 && cur_line[ cur_line_length - 1 ] == '\r' )
+			cur_line_length--;
+	}
+
+public:
+	// return true if no more data is available from input_lines
+	bool eos ( )
+	{
+		if ( cur_field_offset < cur_line_length )
+			return false;
+
+		if ( !input_lines->eos() )
+			return false;
+
+		return true;
+	}
+
+	// line_max is passed to the line_reader, it is also the limit for a full csv row (that may span many lines)
+	explicit csv_reader ( istream &is, char sep=',', char quot='"', unsigned line_max=1<<20 ) :
+		line_max(line_max),
+		line_copy(NULL),
+		sep(sep),
+		quot(quot),
+		cur_line(NULL),
+		cur_line_length(0),
+		cur_line_length_nl(0),
+		cur_field_offset(0)
+	{
+		input_lines = new line_reader(is, line_max);
+	}
+
+	~csv_reader ( )
+	{
+		if ( line_copy )
+			delete[] line_copy;
+
+		delete input_lines;
+	}
+
+	// read one line from input_lines
+	// invalidates previous read_csv_field pointers
+	bool read_line ( )
+	{
+		input_lines->read_line( cur_line, cur_line_length_nl );
+
+		if ( cur_line )
+		{
+			trim_newlines();
+			
+			return true;
+		}
+
+		cur_line_length = cur_line_length_nl = 0;
+
+		return false;
+	}
+
+	// read one csv field from the current line
+	// returns false if no more fields are available in the line, or if there is a syntax error (unterminated quote, end quote followed by neither a quote nor a separator)
+	// returns a pointer to the line start, the offset of the current field, and its length
+	// the 'field_offset' returned by previous calls for the same line is still valid relative to the new 'line_start' (which may change if one field crosses a line boundary, in that case the lines are copied into an internal buffer)
+	bool read_csv_field ( char* &line_start, unsigned &field_offset, unsigned &field_length )
+	{
+		if ( cur_field_offset >= cur_line_length )
+			return false;
+
+		field_offset = cur_field_offset;
+		line_start = cur_line;
+
+		if ( cur_line[ cur_field_offset ] != quot )
+		{
+			// unquoted field
+			char *esep = (char*)memchr( (void*)(cur_line + cur_field_offset), sep, cur_line_length - cur_field_offset );
+
+			if ( esep )
+			{
+				field_length = esep - (cur_line + cur_field_offset);
+				cur_field_offset += field_length + 1;
+			}
+			else
+			{
+				field_length = cur_line_length - cur_field_offset;
+				cur_field_offset = cur_line_length;
+			}
+
+			return true;
+		}
+
+		// quoted field
+		field_length = 1;	// field length until now, including opening quote
+		while (1)
+		{
+			char *equot = NULL;
+
+			if ( cur_field_offset + field_length < cur_line_length )
+				equot = (char*)memchr( (void*)(cur_line + cur_field_offset + field_length), quot,
+						cur_line_length - (cur_field_offset + field_length) );
+
+			if ( equot )
+			{
+				// found end quote
+				field_length = equot - (cur_line + cur_field_offset) + 1;
+
+				if ( cur_field_offset + field_length < cur_line_length )
+				{
+					if ( cur_line[ cur_field_offset + field_length ] == sep )
+					{
+						// end of field
+						cur_field_offset += field_length + 1;
+						return true;
+					}
+
+					if ( cur_line[ cur_field_offset + field_length ] == quot )
+					{
+						// escaped quote
+						field_length++;
+						continue;
+					}
+
+					// syntax error
+					cur_field_offset += field_length;
+					return false;
+				}
+
+				// end quote was at end of line
+				cur_field_offset = cur_line_length;
+				return true;
+			}
+
+			// no closing quote on current input_lines line
+			if ( cur_line != line_copy )
+			{
+				// copy current line to internal buffer
+				if ( !line_copy )
+					line_copy = new char[line_max];
+
+				memcpy( line_copy, cur_line, cur_line_length_nl );
+
+				line_start = cur_line = line_copy;
+			}
+
+			char *next_line = NULL;
+			unsigned next_line_length_nl = 0;
+
+			input_lines->read_line( next_line, next_line_length_nl );
+
+			if ( next_line && cur_line_length_nl + next_line_length_nl <= line_max )
+			{
+				// next line fits in internal buffer: append
+				memcpy( line_copy + cur_line_length_nl, next_line, next_line_length_nl );
+
+				field_length = cur_line_length_nl - cur_field_offset;
+
+				cur_line_length_nl += next_line_length_nl;
+
+				trim_newlines();
+			}
+			else
+			{
+				// reached end of input_lines / line_max with no end quote: return syntax error
+				cur_field_offset = cur_line_length;
+				return false;
+			}
+		}
+	}
+};
 
 static char *outfile = NULL;
 static char sep = ',';
@@ -149,15 +331,20 @@ static bool has_headerline = true;
 
 static void csv_extract( string &colname, istream &input )
 {
-	line_reader lines(input);
+	csv_reader reader( input, sep, quot );
 
-	while ( ! lines.eos() )
+	while ( reader.read_line() )
 	{
-		unsigned char *ptr = NULL;
+		char *ptr = NULL;
+		unsigned off = 0;
 		unsigned len = 0;
-		lines.read_line( ptr, len );
-		if ( ptr )
-			fprintf(stdout, "%3.3s\n", ptr);
+		int i = 0;
+
+		while ( reader.read_csv_field( ptr, off, len ) )
+		{
+			fprintf(stdout, "f%d: %.*s\n", i, len, ptr+off);
+		}
+		fprintf(stdout, "eol\n");
 	}
 }
 
